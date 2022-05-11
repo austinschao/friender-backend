5fcb763fab41c1552afe4f25683915bd7a3d68e2
Austin Chao <austinschao@gmail.com>
2022-05-11 10:07:31 -0700

    AWS file and dog jpeg

--- aws_calls.py
+++ aws_calls.py
@@ -2,10 +2,14 @@ import os
 import boto3
 
 
+
+
 s3 = boto3.client(
   "s3",
   "us-west-1",
-  aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
-  aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
+  aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID'],
+  aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 )
+
+s3.upload_file('brown_dog.jpeg', 'r25-friender', 'brown_dog.jpg')
 
