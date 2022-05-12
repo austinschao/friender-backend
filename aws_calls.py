c9e12c521c92152ce6b4f20b72b239e74a35b1d5
Austin Chao <austinschao@gmail.com>
2022-05-12 16:19:45 -0700

    refactored aws call py

--- aws_calls.py
+++ aws_calls.py
@@ -1,7 +1,7 @@
 import os
 import boto3
 
-ALLOWED_EXTENSIONS = set([ 'png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['jpeg'])
 BASE_URL = f"https://{os.environ['AWS_BUCKET']}.s3.us-west-1.amazonaws.com"
 
 s3 = boto3.client(
@@ -11,15 +11,12 @@ s3 = boto3.client(
   aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
 )
 
-s3.upload_file('brown_dog.jpeg', 'r25-friender-melaus', 'brown_dog.jpg',
-ExtraArgs={"ContentType": 'image/jpeg', "ContentDisposition": 'inline', "ACL": 'public-read'})
-
 def upload_image_and_get_url(file_path, username):
   """ Uploads image to AWS and return object URL"""
-  s3.upload_file(file_path, 'r25-friender-melaus', f"{username}.jpg",
+  s3.upload_file(file_path, os.environ['AWS_BUCKET'], f"{username}.jpeg",
   ExtraArgs={"ContentType": 'image/jpeg', "ContentDisposition": 'inline', "ACL": 'public-read'})
 
-  return f"{BASE_URL}/{username}.jpg"
+  return f"{BASE_URL}/{username}.jpeg"
 
 
 def allowed_file(filename):
