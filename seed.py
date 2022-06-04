aa2daf78b7068e2e774038f57c76f70b6fe0227d
Austin Chao <austinschao@gmail.com>
2022-06-04 10:43:38 -0700

    Commented out match csv. Will only use user csv

--- seed.py
+++ seed.py
@@ -12,14 +12,14 @@ db.create_all()
 with open('generator/users.csv') as users:
     db.session.bulk_insert_mappings(User, DictReader(users))
 
-with open('generator/matches.csv') as matches:
-    db.session.bulk_insert_mappings(Match, DictReader(matches))
+# with open('generator/matches.csv') as matches:
+#     db.session.bulk_insert_mappings(Match, DictReader(matches))
 
-with open('generator/rejects.csv') as rejects:
-    db.session.bulk_insert_mappings(Reject, DictReader(rejects))
+# with open('generator/rejects.csv') as rejects:
+#     db.session.bulk_insert_mappings(Reject, DictReader(rejects))
 
-with open('generator/messages.csv') as messages:
-    db.session.bulk_insert_mappings(Message, DictReader(messages))
+# with open('generator/messages.csv') as messages:
+#     db.session.bulk_insert_mappings(Message, DictReader(messages))
 
 
 
